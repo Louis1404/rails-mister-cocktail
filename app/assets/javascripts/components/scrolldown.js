cac92e6a76eb3624ae81bdaf12757faa9101e752
louisburette <louis.burette@ieseg.fr>
2018-08-17 11:18:20 +0200

    Enable uglifier harmony mode

--- app/assets/javascripts/components/scrolldown.js
+++
@@ -1,9 +0,0 @@
-$(function scrollDownBanner() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
-
-export { scrollDownBanner };
-
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,3 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 import 'bootstrap';
-import { scrollDownBanner  } from '../components/scrolldown';
-console.log('Hello World from Webpacker')
-
-
-
-scrollDownBanner();
